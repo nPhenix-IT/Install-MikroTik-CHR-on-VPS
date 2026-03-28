b963efe0dd308a3e4024dafac828a7353e2d5d27
Delario <delario@phenix-it.com>
2026-03-28 17:37:26 +0000

    Update download link for MikroTik installer

--- mik78.sh
+++ mik78.sh
@@ -6,7 +6,7 @@ echo "=== https://github.com/nPhenix-IT ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
-wget https://github.com/elseif/MikroTikPatch/releases/download/7.18.2/chr-7.18.2.img.zip -O chr.img.zip  && \
+wget https://github.com/elseif/MikroTikPatch/releases/download/7.22.1/chr-7.22.1.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
